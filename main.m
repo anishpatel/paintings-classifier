aae6fcc4ce565a9695620bd7b177135be87f7857
Anish Patel <anishpatel.github@gmail.com>
2013-05-06 10:23:20 -0400

    creates final confusion matrix

--- main.m
+++ main.m
@@ -7,66 +7,66 @@ run_vl_setup
 setParams
 
 %% Read Images - read images from files and pre-process
-load('data/images.mat')
-% fprintf('Reading images\n')
-% [trainImgs, trainLbls, testImgs, testLbls] = getImgs(gaussFilt); % smoothes each image after read
-% trainSmlImgs = getSmallImgs(trainImgs);
-% testSmlImgs = getSmallImgs(testImgs);
-% save('data/images.mat', 'trainImgs', 'testImgs', 'trainLbls', ...
-%     'testLbls', 'trainSmlImgs', 'testSmlImgs')
+% load('data/images.mat')
+fprintf('Reading images\n')
+[trainImgs, trainLbls, testImgs, testLbls] = getImgs(gaussFilt); % smoothes each image after read
+trainSmlImgs = getSmallImgs(trainImgs);
+testSmlImgs = getSmallImgs(testImgs);
+save('data/images.mat', 'trainImgs', 'testImgs', 'trainLbls', ...
+    'testLbls', 'trainSmlImgs', 'testSmlImgs')
 
 %% Train SIFT - generate training SIFT features and build vocabulary
 % load('data/trainSiftFeats.mat')
-% fprintf('Generating training SIFT features\n')
-% [trainSiftDescrs, nTrainSiftFeatsPerImg, trainSiftDescrsPerImg] = getSiftFeats(trainSmlImgs);
-% save('data/trainSiftFeats.mat', 'trainSiftDescrs', ...
-%     'nTrainSiftFeatsPerImg', 'trainSiftDescrsPerImg')
+fprintf('Generating training SIFT features\n')
+[trainSiftDescrs, nTrainSiftFeatsPerImg, trainSiftDescrsPerImg] = getSiftFeats(trainSmlImgs);
+save('data/trainSiftFeats.mat', 'trainSiftDescrs', ...
+    'nTrainSiftFeatsPerImg', 'trainSiftDescrsPerImg')
 
 % Train SIFT Vocab 
 % build a SIFT feature vocabulary and create a SIFT words histogram for each training image
-load('data/trainSiftHists.mat')
-% fprintf('Building sift vocabulary and creating train sift word histograms\n')
-% [siftVocab, trainSiftHists] = getVocab(nTrainImgs, trainSiftDescrs, nTrainSiftFeatsPerImg);
-% save('data/trainSiftHists.mat', 'siftVocab', 'trainSiftHists')
+% load('data/trainSiftHists.mat')
+fprintf('Building sift vocabulary and creating train sift word histograms\n')
+[siftVocab, trainSiftHists] = getVocab(nTrainImgs, trainSiftDescrs, nTrainSiftFeatsPerImg);
+save('data/trainSiftHists.mat', 'siftVocab', 'trainSiftHists')
 
 %% Test SIFT - generate SIFT features for each testing image
 % load('data/testSiftFeats.mat')
-% fprintf('creating test sift features\n')
-% [testSiftDescrs, nTestSiftFeatsPerImg, testSiftDescrsPerImg] = getSiftFeats(testSmlImgs);
-% save('data/testSiftFeats.mat', 'testSiftDescrs', 'nTestSiftFeatsPerImg', 'testSiftDescrsPerImg')
+fprintf('creating test sift features\n')
+[testSiftDescrs, nTestSiftFeatsPerImg, testSiftDescrsPerImg] = getSiftFeats(testSmlImgs);
+save('data/testSiftFeats.mat', 'testSiftDescrs', 'nTestSiftFeatsPerImg', 'testSiftDescrsPerImg')
 
 % Test SIFT Hists - create a SIFT words histogram for each testing image
 % TODO use histc
 % TODO use kd-trees
-load('data/testSiftHists.mat')
-% fprintf('creating test sift word histograms\n')
-% testSiftHists = getSiftWordHists( nTestImgs, siftVocab, ...
-%     testSiftDescrsPerImg, nTestSiftFeatsPerImg);
-% save('data/testSiftHists.mat', 'testSiftHists')
+% load('data/testSiftHists.mat')
+fprintf('creating test sift word histograms\n')
+testSiftHists = getSiftWordHists( nTestImgs, siftVocab, ...
+    testSiftDescrsPerImg, nTestSiftFeatsPerImg);
+save('data/testSiftHists.mat', 'testSiftHists')
 
 %% GLCM Feats
-load('data/GlcmFeats.mat')
-% trainGlcmFeats = getGlcmFeats(trainImgs);
-% testGlcmFeats = getGlcmFeats(testImgs);
-% save('data/GlcmFeats.mat', 'trainGlcmFeats', 'testGlcmFeats')
-
-%% HSV Hists - calculate HSV histograms for each image
-load('data/HsvHists.mat')
-% trainHsvHists = getHsvHists(trainImgs);
-% testHsvHists = getHsvHists(testImgs);
-% save('data/HsvHists.mat', 'trainHsvHists', 'testHsvHists')
+% load('data/GlcmFeats.mat')
+trainGlcmFeats = getGlcmFeats(trainImgs);
+testGlcmFeats = getGlcmFeats(testImgs);
+save('data/GlcmFeats.mat', 'trainGlcmFeats', 'testGlcmFeats')
 
 %% RGB Hists - calculate RGB histograms for each image
-load('data/RgbHists.mat')
-% trainRgbHists = getRgbHists(trainImgs);
-% testRgbHists = getRgbHists(testImgs);
-% save('data/RgbHists.mat', 'trainRgbHists', 'testRgbHists')
+% load('data/RgbHists.mat')
+trainRgbHists = getRgbHists(trainImgs);
+testRgbHists = getRgbHists(testImgs);
+save('data/RgbHists.mat', 'trainRgbHists', 'testRgbHists')
 
-%% Line Feats
-load('data/LineFeats.mat')
-% trainLineFeats = getLineFeats(trainImgs);
-% testLineFeats = getLineFeats(testImgs);
-% save('data/LineFeats.mat', 'trainLineFeats', 'testLineFeats')
+%% HSV Hists - calculate HSV histograms for each image
+% load('data/HsvHists.mat')
+trainHsvHists = getHsvHists(trainImgs);
+testHsvHists = getHsvHists(testImgs);
+save('data/HsvHists.mat', 'trainHsvHists', 'testHsvHists')
+
+%% Line Feats - calculates number of lines and avg. line length
+% load('data/LineFeats.mat')
+trainLineFeats = getLineFeats(trainImgs);
+testLineFeats = getLineFeats(testImgs);
+save('data/LineFeats.mat', 'trainLineFeats', 'testLineFeats')
 
 %% Edge Hists - calculate egde gradient histograms for each image
 % load('data/EdgeHists.mat')
@@ -84,9 +84,9 @@ predMat = true(nTestImgs, nLbls);
 truePos = zeros(nLbls, 1);
 
 %% Abstract Cubism vs. Impressionism Pointillism Renaissance Surrealism
-trueLblNums  = [I P];
-falseLblNums = [A C R S];
-histCols_ACvIPRS = lineCols;
+trueLblNums  = [A C];
+falseLblNums = [I P R S];
+histCols_ACvIPRS = siftCols|glcmCols|rgbCols|hsvCols|lineCols;
 
 classifier_ACvIPRS = getClassifier( ...
     trainHists(:, histCols_ACvIPRS), trainLbls, trueLblNums, falseLblNums );
@@ -97,7 +97,7 @@ fprintf('%7s ', 'ACvIPRS')
     testLbls, trueLblNums, falseLblNums, predMat );
 
 %% Impressionism Pointillism vs. Renaissance Surrealism
-trueLblNums  = [A C];
+trueLblNums  = [I P];
 falseLblNums = [R S];
 histCols_IPvRS = siftCols|glcmCols|rgbCols|hsvCols|lineCols;
 
@@ -155,3 +155,18 @@ for L = labels
 end
 totalAcc = sum(truePos) / nTestImgs;
 fprintf('Total Accuracy: %.02f%%\n', totalAcc*100)
+
+predLbls = zeros(nTestImgs, 1);
+for i = 1:nTestImgs
+    for L = 1:nLbls
+        if predMat(i, L)
+            predLbls(i) = L;
+            break
+        end
+    end
+end
+
+[confMat, order] = confusionmat(testLbls, predLbls);
+figure, imagesc(confMat, [1 50]), colorbar
+
+
